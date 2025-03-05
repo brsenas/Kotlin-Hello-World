63fbffa8567d9df50be371630b2a716b9967fb57
Bruno Sena <brunosen4s@gmail.com>
2025-03-05 20:30:38 -0300

    manipulacao de listas e extension functions

--- src/main/kotlin/brsenas/com/github/helloworld/model/Gamer.kt
+++ src/main/kotlin/brsenas/com/github/helloworld/model/Gamer.kt
@@ -1,18 +1,24 @@
 package brsenas.com.github.helloworld.model
 
+import java.util.Scanner
 import kotlin.random.Random
+import kotlin.time.measureTime
 
 data class Gamer(var nome: String, var email: String) {
     var dataNascimento: String? = null
     var usuario: String? = null
-        set(value){
+        set(value) {
             field = value
-            if(idInterno.isNullOrBlank()){
+            if (idInterno.isNullOrBlank()) {
                 criarIdInterno()
             }
         }
     var idInterno: String? = null
-    private set
+        private set
+
+    //lista de jogos que podem ser alterados
+    val jogosBuscados = mutableListOf<Jogo?>()
+
 
     constructor(nome: String, email: String, dataNascimento: String, usuario: String) :
             this(nome, email) {
@@ -22,18 +28,18 @@ data class Gamer(var nome: String, var email: String) {
     }
 
     //init (bloco de código que é executado antes da execução da classe)
-    init {
-        if (nome.isBlank()){
-            throw IllegalArgumentException("Nome está em branco")
-        }
-        this.email = validarEmail()
-    }
+//    init {
+//        if (nome.isBlank()) {
+//            throw IllegalArgumentException("Nome está em branco")
+//        }
+//        this.email = validarEmail()
+//    }
 
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
 
-    fun criarIdInterno(){
+    fun criarIdInterno() {
         val numero = Random.nextInt(10000)
         val tag = String.format("%04d", numero)
 
@@ -41,13 +47,37 @@ data class Gamer(var nome: String, var email: String) {
     }
 
     //metodo para validar email
-    fun validarEmail(): String{
+    fun validarEmail(): String {
         val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
-            if(regex.matches(email)){
-                return email
-            }else{
-                throw IllegalArgumentException("Email inválido")
-            }
+        if (regex.matches(email)) {
+            return email
+        } else {
+            throw IllegalArgumentException("Email inválido")
+        }
 
+    }
+
+    //metodo estatico para criar game
+
+    companion object {
+        fun criarGame(leitura: Scanner): Gamer {
+            println("Boas vindas ao Brunao Games! Vamos fazer seu cadastro. Digite seu nome:")
+            val nome = leitura.nextLine()
+            println("Digite seu e-mail:")
+            val email = leitura.nextLine()
+            println("Deseja completar seu cadastro com usuário e data de nascimento? (S/N)")
+            val opcao = leitura.nextLine()
+
+            if (opcao.equals("s", true)) {
+                println("Digite sua data de nascimento(DD/MM/AAAA):")
+                val nascimento = leitura.nextLine()
+                println("Digite seu nome de usuário:")
+                val usuario = leitura.nextLine()
+
+                return Gamer(nome, email, nascimento, usuario)
+            } else{
+                return Gamer(nome, email)
+            }
+        }
     }
 }
--- src/main/kotlin/brsenas/com/github/helloworld/principal/Main.kt
+++ src/main/kotlin/brsenas/com/github/helloworld/principal/Main.kt
@@ -1,42 +1,91 @@
 package brsenas.com.github.helloworld.principal
 
+import brsenas.com.github.helloworld.model.Gamer
 import brsenas.com.github.helloworld.model.Jogo
 import brsenas.com.github.helloworld.services.ConsumoApi
+import transformarEmIdade
 import java.util.*
 
 fun main() {
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar: ")
-    val busca = leitura.nextLine()
 
-    val buscaApi = ConsumoApi()
-    val informacaoJogo = buscaApi.buscaJogo(busca)
+    val gamer = Gamer.criarGame(leitura)
+    println(
+        "Cadastro concluído com sucesso!\n" +
+                "Dados do Gamer:"
+    )
+    println(gamer)
+    println("Idade do gamer: " + gamer.dataNascimento?.transformarEmIdade())
 
-    var meuJogo: Jogo? = null
+    do {
+        println("Digite um código de jogo para buscar: ")
+        val busca = leitura.nextLine()
 
-    val resultado = runCatching {
-         meuJogo = Jogo(informacaoJogo.info.title,
-            informacaoJogo.info.thumb)
-    }
-    resultado.onFailure {
-        println("Jogo inexistente. Tente outro id")
-    }
-    resultado.onSuccess {
-        println("Deseja inserir uma descricao personalizada? S/N")
-        val opcao = leitura.nextLine()
-        if(opcao.equals("s", true)){
-            println("Insira a descrição personalizada para o jogo: ")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        }else{
-            meuJogo?.descricao = meuJogo?.titulo
+        val buscaApi = ConsumoApi()
+        val informacaoJogo = buscaApi.buscaJogo(busca)
+
+        var meuJogo: Jogo? = null
+
+        val resultado = runCatching {
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb
+            )
+        }
+        resultado.onFailure {
+            println("Jogo inexistente. Tente outro id")
         }
 
         resultado.onSuccess {
-            println("Busca finalizada com sucesso")
-        }
+            println("Deseja inserir uma descricao personalizada? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição personalizada para o jogo: ")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+            gamer.jogosBuscados.add(meuJogo)
 
-        println(meuJogo)
+        }
+        println("Deseja buscar um novo jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("s", true))
+
+    println("Jogos buscados:")
+    println(gamer.jogosBuscados)
+
+    println("Jogos ordenados por título")
+    gamer.jogosBuscados.sortBy {
+        it?.titulo
     }
+
+    gamer.jogosBuscados.forEach {
+        println("Título "+it?.titulo)
+    }
+
+    val jogosFiltrados = gamer.jogosBuscados.filter {
+        it?.titulo?.contains("batman", true) ?: false
+    }
+
+    println("Jogos Filtrados\n" +
+            jogosFiltrados)
+
+    println("Deseja excluir algum jogo da lista original? S/N")
+    val opcao = leitura.nextLine()
+    if (opcao.equals("s", true)){
+        println(gamer.jogosBuscados)
+        println("Informe a posicao do jogo que deseja excluir")
+        val posicao = leitura.nextInt()
+        gamer.jogosBuscados.removeAt(posicao)
+    }
+
+    println("Lista Atualizada:\n" +
+            gamer.jogosBuscados)
+
+
+    println("Busca finalizada com sucesso")
 
 }
